a526a3ef051d89d5d0f20dc9d69e632289b2c458
Asilver-jpg <asilver7493@gmail.com>
2020-05-05 21:05:35 -0400

    get list works

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,4 +1,5 @@
 class ListSerializer < ActiveModel::Serializer
-  attributes :id, :item_lists, :name
+  attributes :id, :item_lists, :name, :items
   has_many :item_lists
+  has_many :items, through: :item_lists
 end
